93a58f0f96b6add22ebd2b29389504e0889f5b28
KTZHANG <ktzhang@sfu.ca>
2024-10-10 12:19:44 -0700

    a

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 var express = require('express');
+
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+
 var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
 var wav = require('wav');
@@ -14,7 +34,7 @@ app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/public'))
 
-binaryServer = BinaryServer({ host: `wss://localhost`, port: 9001});
+binaryServer = BinaryServer({ host: `wss://${results["en0"][0]}`, port: 9001});
 
 binaryServer.on('connection', function (client) {
     console.log('new connection');
