06a1d405316c1040bc1cf4c12508982640f77594
KTZHANG <ktzhang@sfu.ca>
2024-10-10 10:50:52 -0700

    fix

--- public/recorder.js
+++ public/recorder.js
@@ -1,6 +1,6 @@
 (function (window) {
     var server = "conv-count-poc-997c48b4c4cc.herokuapp.com"
-    var client = new BinaryClient(`wss://null`);
+    var client = new BinaryClient(`wss://${server}:9001`);
 
     console.log("Test1");
     client.on('open', function() {
