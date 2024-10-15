c9726c442566ce37b3ecbf30a54ab80485a7607d
kenny zhang <ktzhang@sfu.ca>
2024-10-14 17:04:01 -0700

    a

---
+++ python_server.py
@@ -0,0 +1,18 @@
+def play_data(filename, first_sec, second_sec):
+  import ao
+  from ao import AudioDevice 
+  dev = AudioDevice(2, bits=16, rate=16000,channels=1)
+  f = open(filename, 'r')
+  data_len = (second_sec-first_sec)*32000
+  f.seek(32000*first_sec)
+  data = f.read(data_len)
+  dev.play(data)
+  f.close()
+
+
+import urllib.request
+url = 'https://conv-count-poc-997c48b4c4cc.herokuapp.com/str'
+with urllib.request.urlopen(url) as f:
+    html = f.read()
+
+print(html)
