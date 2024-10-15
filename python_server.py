e2f3adad9bf9dd7587261c30c7ff8b10c4f0e484
kenny zhang <ktzhang@sfu.ca>
2024-10-14 20:18:00 -0700

    a

--- python_server.py
+++ python_server.py
@@ -1,24 +1,18 @@
-def play_data(filename, first_sec, second_sec):
-  import ao
-  from ao import AudioDevice 
-  dev = AudioDevice(2, bits=16, rate=16000,channels=1)
-  f = open(filename, 'r')
-  data_len = (second_sec-first_sec)*32000
-  f.seek(32000*first_sec)
-  data = f.read(data_len)
-  dev.play(data)
-  f.close()
-
-
 import requests
 import numpy as np
+
+import simpleaudio as sa
 
 while (True):
     url = 'https://conv-count-poc-997c48b4c4cc.herokuapp.com/str'
 
-    r = requests.get(url, stream=True)
+    r = requests.get(url, stream=False)
     err = False;
 
-    for line in r.iter_lines():
-        len(line)
+    try:
+      data = np.frombuffer(r.content, dtype=int)
+      print(data)
+      play_obj = sa.play_buffer(data, 1, 2, 44000)
+    except:
+      err = True
 
