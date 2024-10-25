9d0ca80ed7a77c5cad479c4d9cc501aa5a711426
kenny zhang <ktzhang@sfu.ca>
2024-10-25 14:24:14 -0700

    export data

--- public/recorder.js
+++ public/recorder.js
@@ -60,9 +60,9 @@
         timeElapsed += 1
         ms.value = timeElapsed;
 
-        var ax = threshold(accelVectors[0], 0.8)
-        var ay = threshold(accelVectors[1], 0.8)
-        var az = threshold(accelVectors[2], 0.8)
+        var ax = threshold(accelVectors[0], 0.4)
+        var ay = threshold(accelVectors[1], 0.4)
+        var az = threshold(accelVectors[2], 0.4)
 
         avx.value = ax
         avy.value = ay
