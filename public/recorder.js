42f3258d55a2ab17b7992e9ec7ab4ff00faeb7fc
kenny zhang <ktzhang@sfu.ca>
2024-11-03 15:22:17 -0800

    mapped diff algo to use 0.5sin for smoothing

--- public/recorder.js
+++ public/recorder.js
@@ -29,7 +29,7 @@
     var testBeta = [];
     var testGamma = [];
 
-    const rotDelta = 100;
+    const rotDelta = 0.5;
 
     console.log("Test1");
 
@@ -97,11 +97,11 @@
             axc += accelVectors[0]; ayc += accelVectors[1]; azc += accelVectors[2];
 
             if (timeElapsed % 2 == 1) {
-                diff -= compass;
+                diff -= Math.sin(0.5*((compass * Math.PI)/180));
                 avgZ += abs(diff);
             }
             else {
-                diff = compass;
+                diff = Math.sin(0.5*((compass * Math.PI) / 180));
             }
 
             timeElapsed += 1
