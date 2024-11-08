d216b2ceae5231e5321bbcd16e2003a030ac2c58
KTZHANG <ktzhang@sfu.ca>
2024-11-08 15:12:37 -0800

    stream

--- app.js
+++ app.js
@@ -124,11 +124,11 @@ binaryServer.on('connection', function (client) {
     console.log('new connection');
     try {
         client.on('stream', function (stream, meta) {
-            console.log('new stream at' + needle);
+           // console.log('new stream at' + needle);
             channels[needle] = stream;
 
             stream.on('end', function () {
-                console.log('end stream at' + needle);
+               // console.log('end stream at' + needle);
              //   pipeM = null;
             });
         });
