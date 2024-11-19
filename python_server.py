7ede8759ed9df25b544d59bfed6ece8b3058b71b
KTZHANG <ktzhang@sfu.ca>
2024-11-18 16:12:43 -0800

    Add cluster and transcript generation

--- python_server.py
+++ python_server.py
@@ -13,15 +13,59 @@ stream = p.open(format=pyaudio.paFloat32,
                 )
 
 base = 65535
-while (True):
-    url = 'https://conv-count-poc-997c48b4c4cc.herokuapp.com/convo-ts-logs'
 
+def error(exp, actual):
+   if (exp == 0):
+      return 0;
+
+   return abs(exp - actual)/abs(exp);
+
+def cluster(df, err):
+   pcxy = 0
+   clusters = []
+   ind = -1
+   for index, row in df.sort_values(by=['cxy']).iterrows():
+      if error(pcxy,row['cxy']) > err:
+        clusters.append([row['id']])
+        ind += 1
+      else:
+        if ind == -1:
+          clusters.append([row['id']])
+          ind += 1
+        else:
+          clusters[ind].append(row['id'])
+
+      pcxy = row['cxy']
+   return clusters;
+
+def transcripts(data, clusters):
+   datav = data.sort_values(by=['timestamp'])
+   convos = []
+   for ids in clusters:
+      convo = []
+      for id in ids:
+         convo.append(datav.loc[datav['id'] == id]['transcript'])
+
+      convos.append(convo)
+  
+   return convos;
+
+while (True):
+    url = 'https://conv-count-poc-997c48b4c4cc.herokuapp.com/convo-ts-ids'
+    url2 = 'https://conv-count-poc-997c48b4c4cc.herokuapp.com/convo-ts-logs'
     r = requests.get(url, stream=False)
+    r2 = requests.get(url2, stream=False)
     err = False;
 
+
     try:
-      data = pd.read_json(r.content);
-      print(data)
+      data = pd.read_json(r.text);
+      data2 = pd.read_json(r2.text);
+      c = cluster(data, 0.5);
+    
+      ts = transcripts(data2, c);
+    
+      print(ts)
     except:
       err = True
 
