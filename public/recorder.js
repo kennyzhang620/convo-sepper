703f73a0bf222a91977fdfc27e4e524061197082
kenny zhang <ktzhang@sfu.ca>
2024-11-19 20:46:12 -0800

    mapped diff algo to use 0.5sin for smoothing

--- public/recorder.js
+++ public/recorder.js
@@ -13,7 +13,7 @@
     var pvx = document.getElementById("px");
     var pvz = document.getElementById("pz");
 
-    const bufferT = 2000;
+    const bufferT = 50;
     var recording = false;
 
     var accelVectors = [0, 0, 0]
