af7127e123497db8e9242cfbeafa1a603e695a67
KTZHANG <ktzhang@sfu.ca>
2024-11-30 14:53:23 -0800

    ui button

--- public/convolist.js
+++ public/convolist.js
@@ -61,7 +61,7 @@ function generateCell(res, max_size) {
 
 			var html = `<div class="img_header" style='border: 1px solid black; border-radius: 10px; background-color: ${checkPaused(res[i].ids) ? "yellow" : "white"}'>
 							<div class="text_content">
-                            <div id="header"><div id="title_header" style="padding:6px; width: 90%;">${res[i].inference}</div><div id="back" style="background-color: black;width: 30px;height: 30px;display: flex; float: right; border-radius: 30px;" onclick='loadCell${i}'></div></div>
+                            <div id="header"><div id="title_header" style="padding:6px; width: 90%;">${res[i].inference}</div><div id="back" style="background-color: black;width: 30px;height: 30px;display: flex; float: right; border-radius: 30px;" onclick='loadCell(${i})'></div></div>
                             <div id="ts${i}" style="display: none;">    
                                 <h3>Transcript</h3>
                                     ${generateConvoList(res[i].transcript.trim().split("\n"), max_size)}
