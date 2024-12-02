531bb466153e34acdf03b677c0c8693e31a9d544
KTZHANG <ktzhang@sfu.ca>
2024-12-01 16:34:58 -0800

    change ui

--- public/convolist.js
+++ public/convolist.js
@@ -61,7 +61,7 @@ function generateCell(res, max_size) {
 			//console.log("res1: ", res[i].Project)
 			var filler = `<div id="filler" style="width: 100px; height: 100px;"></div>`;
 
-			var html = `<div class="img_header" style='border: 1px solid black; border-radius: 10px; background-color: ${checkPaused(res[i].ids) ? "white" : fform_colors.get(res[i].convo_id)`).style.backgroundColor}'>
+			var html = `<div class="img_header" style='border: 1px solid black; border-radius: 10px; background-color: ${checkPaused(res[i].ids) ? "white" : fform_colors.get(res[i].convo_id)}'>
 							<div class="text_content">
                             <div id="header"><div id="title_header-${res[i].convo_id}" style="padding:6px; width: 90%;">${res[i].inference}</div><div id="back" style="background-color: black;width: 30px;height: 30px;display: flex; float: right; border-radius: 30px; position:relative; bottom: 40px;" onclick='loadCell(${res[i].convo_id})'></div></div>
                             <div id="ts${res[i].convo_id}" style="display: none;">    
